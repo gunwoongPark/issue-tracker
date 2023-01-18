4b27c0f4da83355c53472f55e1d5923c1160d2b0
gunwoongPark <bung1438@gmail.com>
2023-01-18 17:50:50 +0900

    feat: custom axios instance 생성 및 설정

---
+++ src/lib/api/index.ts
@@ -0,0 +1,36 @@
+import axios, { AxiosRequestConfig } from "axios";
+import { config as basedConfig } from "../config";
+
+export const axiosInstance = axios.create({
+  baseURL: basedConfig.apiBaseUri,
+});
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    config.headers = config.headers ?? {};
+    config.headers.Authorization = `Bearer ${basedConfig.apiToken}`;
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  async (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+const apiBase = {
+  get: async (url: string, config?: AxiosRequestConfig<any>) => {
+    const res = await axiosInstance.get(url, config);
+    return res.data;
+  },
+};
+
+export default apiBase;
---
+++ src/lib/config.ts
@@ -0,0 +1,4 @@
+export const config = {
+  apiBaseUri: process.env.REACT_APP_API_BASE_URI,
+  apiToken: process.env.REACT_APP_API_TOKEN,
+};
