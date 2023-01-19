455623b2a7ceb3442aacbfaeba977afb68637d20
gunwoongPark <bung1438@gmail.com>
2023-01-19 15:32:23 +0900

    refactor: 불필요 컴포넌트화 제거

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,12 +1,11 @@
 import { Link, useSearchParams } from "react-router-dom";
 import InputView from "../components/InputView";
-import PaginationButtonView from "../components/PaginationButtonView";
 import RepoItemView from "../components/RepoItemView";
 import useSearch from "../hooks/react-query/useSearch";
 
 const SearchPage = () => {
   // query string
-  const searchParams = useSearchParams()[0];
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const searchRepoName = searchParams.get("q");
   const page = Number(searchParams.get("page")) || 1;
@@ -35,7 +34,24 @@ const SearchPage = () => {
         </ul>
       )}
 
-      <PaginationButtonView itemList={searchRepoList} />
+      <button
+        onClick={() => {
+          searchParams.set("page", String(page - 1));
+          setSearchParams(searchParams);
+        }}
+        disabled={page === 1}
+      >
+        Prev
+      </button>
+      <button
+        onClick={() => {
+          searchParams.set("page", String(page + 1));
+          setSearchParams(searchParams);
+        }}
+        disabled={searchRepoList.length < 15}
+      >
+        Next
+      </button>
     </>
   );
 };
