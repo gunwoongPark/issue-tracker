7df6fef29ee80083b5ae66f1f20f1bbfe24a3dfa
gunwoongPark <bung1438@gmail.com>
2023-01-25 01:57:29 +0900

    refactor: header view 컴포넌트의 home button -> link로 수정

--- src/components/common/HeaderView.tsx
+++ src/components/common/HeaderView.tsx
@@ -3,13 +3,11 @@ import styled, { css } from "styled-components";
 import { themeContext } from "../../context/CustomThemeProvider";
 import InputView from "./InputView";
 import { BiMoon, BiSun, BiHomeAlt } from "react-icons/bi";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const HeaderView = () => {
   // context
   const context = useContext(themeContext);
-  // navigate
-  const navigate = useNavigate();
   // location
   const location = useLocation();
 
@@ -23,9 +21,9 @@ const HeaderView = () => {
     <S.Container isHomePage={isHomePage}>
       <div className="button-container">
         {!isHomePage && (
-          <button className="button home-button" onClick={() => navigate("/")}>
+          <Link to="/" className="button home-button">
             <BiHomeAlt size={24} color="#ffffff" />
-          </button>
+          </Link>
         )}
 
         <label className="button" htmlFor="theme-toggle-button">
@@ -83,10 +81,6 @@ const S = {
         justify-content: center;
         align-items: center;
         cursor: pointer;
-      }
-
-      .home-button {
-        border: none;
       }
 
       #theme-toggle-button {
