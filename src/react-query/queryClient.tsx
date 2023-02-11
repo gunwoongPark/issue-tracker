c2d6d035aa23a504548efd4a03d075647a8e116c
gunwoongPark <bung1438@gmail.com>
2023-02-12 01:28:37 +0900

    fix: react-query staleTime, cacheTime 수정

--- src/react-query/queryClient.tsx
+++ src/react-query/queryClient.tsx
@@ -5,7 +5,8 @@ export const queryClient = new QueryClient({
     queries: {
       refetchOnWindowFocus: false,
       retry: 0,
-      staleTime: 1000 * 30,
+      staleTime: 1000 * 60 * 10,
+      cacheTime: 1000 * 60 * 15,
     },
   },
 });
